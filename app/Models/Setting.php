8ee16877657208f35d8168942203692de30686a3
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-13 12:26:02 +0000

    Setup setting management

---
+++ app/Models/Setting.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Config;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public static function get($key)
+    {
+        $setting = new self();
+
+        $entry = $setting->where('key', $key)->first();
+        if (!$entry) {
+            return;
+        }
+
+        return $entry->value;
+    }
+
+    public function set($key, $value = null)
+    {
+        $setting = new self();
+
+        $entry = $setting->where('key', $key)->firstOrFail();
+        $entry->value = $value;
+        $entry->saveOrFail();
+
+        Config::set($key, $value);
+        if (Config::get($key) !== $value) {
+            return false;
+        }
+
+        return true;
+    }
+}
