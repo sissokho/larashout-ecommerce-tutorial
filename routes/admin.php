5af4e6cca830ae64dabd0eabec11adae45e03611
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-11-15 11:31:42 +0000

    Manage settings

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\SettingController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')
@@ -10,7 +11,12 @@ Route::prefix('admin')
         Route::post('login', [LoginController::class, 'login'])->name('login.post');
         Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
-        Route::get('/', function () {
-            return view('admin.dashboard.index');
-        })->name('dashboard')->middleware('auth:admin');
+        Route::middleware(['auth:admin'])->group(function () {
+            Route::get('/', function () {
+                return view('admin.dashboard.index');
+            })->name('dashboard');
+
+            Route::get('/settings', [SettingController::class, 'index'])->name('settings');
+            Route::post('/settings', [SettingController::class, 'update'])->name('settings.update');
+        });
     });
